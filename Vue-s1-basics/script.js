35a249f2fc827823f8df046677102a924b323f6d
Esther Bauzou <48768205+PetitChampi@users.noreply.github.com>
2022-03-10 14:37:17 +0100

    first commit

---
+++ Vue-s1-basics/script.js
@@ -0,0 +1,49 @@
+Vue.createApp({
+  data() {
+    return {
+      plants: [
+        {
+          name: 'adromischus cristatus zeyheri',
+          imgUrl: '../assets/imgs/plants/adromischus-cristatus-zeyheri.jpg',
+          price: 4.25,
+          isOnSale: true
+        },
+        {
+          name: 'adromischus filicaulis',
+          imgUrl: '../assets/imgs/plants/adromischus-filicaulis.jpg',
+          price: 3.95,
+          isOnSale: false
+        },
+        {
+          name: 'adromischus marlothii',
+          imgUrl: '../assets/imgs/plants/adromischus-marlothii.jpg',
+          price: 3,
+          isOnSale: true
+        },
+        {
+          name: 'adromischus triflorus',
+          imgUrl: '../assets/imgs/plants/andromischus-triflorus-calico-hearts.jpg',
+          price: 4.1,
+          isOnSale: false
+        },
+        {
+          name: 'adromischus cristatus',
+          imgUrl: '../assets/imgs/plants/andromiscus-cristatus.jpg',
+          price: 5.3,
+          isOnSale: false
+        }
+      ],
+      showCheaperPlants: true
+    }
+  },
+  methods: {
+    toggleCheaperPlants() {
+      this.showCheaperPlants = !this.showCheaperPlants
+    }
+  },
+  computed: {
+    plantsUnder4Euro() {
+      return this.plants.filter(plant => plant.price < 4)
+    }
+  }
+}).mount('#app')
